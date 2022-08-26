342daf11626ecceb056dc18ff43dc50ab71e250b
tgsat <satrio.pragma@gmail.com>
2022-08-26 18:22:57 +0700

    Initial Commit

---
+++ Coffee Shop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coffee Shop
+//
+//  Created by Teguh Satrio on 8/26/22.
+//  Copyright © 2022 tgsatt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
