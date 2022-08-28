cb4498478761033136a5a03216323b9a06ca6470
tgsat <satrio.pragma@gmail.com>
2022-08-28 17:53:48 +0700

    initial commit

---
+++ Coffee Shop/View/Utils/Dictionary.swift
@@ -0,0 +1,9 @@
+//
+//  Dictionary.swift
+//  Coffee Shop
+//
+//  Created by Teguh Satrio on 8/27/22.
+//  Copyright © 2022 tgsatt. All rights reserved.
+//
+
+import Foundation
---
+++ Coffee Shop/View/Utils/MyColor.swift
@@ -0,0 +1,9 @@
+//
+//  MyColor.swift
+//  Coffee Shop
+//
+//  Created by Teguh Satrio on 8/26/22.
+//  Copyright © 2022 tgsatt. All rights reserved.
+//
+
+import Foundation
